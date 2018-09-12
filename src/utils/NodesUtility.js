5a18fe23d73938108384111915d276d616d9a061
Lucas Nowak <lucas.nowak@ahoii.net>
2018-09-12 20:54:14 +0200

    Cleanup code

--- src/utils/NodesUtility.js
+++ src/utils/NodesUtility.js
@@ -1,15 +1,19 @@
 import nodes from '../constants/nodes'
 
 /**
- *
- * @param graph
- * @param start
- * @param target
+ * pathfinding based on the dijkstra algorithm (by Edsger W. Dijkstra)
+ * @param graph: array
+ * @param start: number
+ * @param target: number
  * @return {{path: Array, costs: number}}
  */
 export const dijkstra = (graph, start, target) => {
 	let startNode = getNode(nodes, start),
 		visitedNodes = [],
+
+		// take every node in graph and set the costs to start node
+		// every node costs are infinity except start
+		// start distance to start is 0
 		nodeList = graph.map(node => ({
 			'id': +node.id,
 			'parent': +node.id,
@@ -17,7 +21,7 @@ export const dijkstra = (graph, start, target) => {
 		})
 	)
 
-
+	// runs until every node is calculated and save in visitedNodes
 	let notCompleted = true
 	while (notCompleted) {
 		notCompleted = nodeList.filter(e => !visitedNodes.includes(e.id)).length
@@ -25,12 +29,16 @@ export const dijkstra = (graph, start, target) => {
 
 		nodeList = sortByDistanceToStart(nodeList)
 
+		// investigatedNode is the node with the lowest costs to start
 		let investigatedNode = nodeList.filter(e => !visitedNodes.includes(e.id))[0],
 			unvisitedNeighbors = getUnvisitedNeighbors(graph, investigatedNode.id, visitedNodes)
 
+		// calculate the costs of unvisitedNeighbors to start
 		unvisitedNeighbors.forEach(neighbor => {
 			let newCosts = investigatedNode.costs + neighbor.distance,
 				oldCosts = nodeList.find(e => e.id == neighbor.to).costs
+
+			// update if new costs lower
 			if (newCosts < oldCosts) {
 				let index = nodeList.findIndex(e => e.id == neighbor.to)
 				nodeList[index].parent = investigatedNode.id
@@ -38,6 +46,7 @@ export const dijkstra = (graph, start, target) => {
 			}
 		})
 
+		// mark node as visited | add to visitedNodes
 		visitedNodes.push(investigatedNode.id)
 	}
 
@@ -45,20 +54,19 @@ export const dijkstra = (graph, start, target) => {
 }
 
 /**
- *
- * @param graph
- * @param id
- * @returns {number | * | T | {}}
+ * @param graph: array
+ * @param id: number
+ * @returns {{node}}
  */
 export const getNode = (graph, id) => graph.find(e => e.id == id)
 
 
 /**
- *
- * @param graph
- * @param node
- * @param visitedNeighbors
- * @returns {*}
+ * return list of unvisited neighbors
+ * @param graph: array
+ * @param node: {}
+ * @param visitedNeighbors: array
+ * @returns {array}
  */
 const getUnvisitedNeighbors = (graph, node, visitedNeighbors) => {
 	node = getNode(graph, node)
@@ -68,17 +76,16 @@ const getUnvisitedNeighbors = (graph, node, visitedNeighbors) => {
 }
 
 /**
- *
- * @param list
- * @returns {DependencyReference[]|void|*}
+ * @param list: array
+ * @return {array}
  */
 const sortByDistanceToStart = list => list.sort((a, b) => a.costs - b.costs)
 
 /**
- *
- * @param list
- * @param start
- * @param target
+ * return best path and way costs
+ * @param list: array
+ * @param start: number | string
+ * @param target: number | string
  * @returns {{path: Array, costs: number}}
  */
 const getPath = (list, start, target) => {
@@ -86,6 +93,7 @@ const getPath = (list, start, target) => {
 		path = [],
 		costs = 0
 
+	// add way points id and calculate costs
 	while (currentId != start) {
 		let next = list.find(e => e.id == currentId)
 
@@ -93,6 +101,8 @@ const getPath = (list, start, target) => {
 		currentId = next.parent
 		costs += next.costs
 	}
+
+	// add start id
 	path.unshift(+start)
 
 	return {
@@ -102,8 +112,7 @@ const getPath = (list, start, target) => {
 }
 
 /**
- *
- * @param id
- * @return {*}
+ * @param id: number | string
+ * @return {{latitude: number, longitude: number}}
  */
 export const getCoordinates = id => nodes.find(e => e.id == id).coordinates
