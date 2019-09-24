0a989d7300c25a432854b8b5ed89e5fa3db761a1
Lucas Nowak <lucas.nowak@spring-media.de>
2019-09-24 09:56:06 +0200

    Update dependencies
    Update cold dependencies and fix legacy errors.
    Add prettiertc for code styling.

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,49 @@
 const path = require('path'),
-	webpack = require('webpack'),
-	htmlWebpackPlugin = require('html-webpack-plugin'),
-	CleanWebpackPlugin = require('clean-webpack-plugin')
+  webpack = require('webpack'),
+  htmlWebpackPlugin = require('html-webpack-plugin'),
+  { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-	entry: './src/App.js',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: "babel-loader"
-				}
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					{ loader: 'style-loader' },
-					{ loader: 'css-loader' },
-					{ loader: 'sass-loader' }
-				]
-			}
-		]
-	},
-	devServer: {
-		historyApiFallback: true,
-		hot: true,
-		stats: 'minimal',
-		overlay: {
-			warnings: true,
-			errors: true
-		}
-	},
-	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new htmlWebpackPlugin({
-			template: './src/index.html'
-		}),
-		new webpack.HotModuleReplacementPlugin()
-	],
-	output: {
-		filename: 'app.js',
-		path: path.join(__dirname, '/docs')
-	}
-};
+  entry: './src/App.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    stats: 'minimal',
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['./dist'],
+    }),
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, '/docs'),
+  },
+}
